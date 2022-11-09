6988cd7ad438f6efe0cb1bedc10e83b358c37545
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-11-09 11:05:46 -0500

    API returns lat & lon coordinates

---
+++ src/utils/ResolveSearchInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ResolveSearchInput = (location) => {
+  // RegExp to match either zip or city name
+  const regexZip = new RegExp(
+    /^((\d{5}-\d{4})|(\d{5})|([A-Z]\d[A-Z]\s\d[A-Z]\d))$/
+  );
+  const regexCity = new RegExp(
+    /^([a-zA-Z\u0080-\u024F]+(?:(. )|-| |'))*[a-zA-Z\u0080-\u024F]*$/
+  );
+
+  // Remove spaces from input
+  let temp = location
+    .replace(/(\s+$|^\s+)/g, '') //remove any spaces before and after
+    //   .replace(/(,\s+)/g, ",")
+    //   .replace(/(\s+,)/g, ",")
+    .replace(/\s+/g, '%20'); // replaces spaces with plus
+
+  let locationForApi;
+  if (temp !== '') {
+    if (regexZip.test(temp)) {
+      locationForApi = `name=${temp}`;
+    } else if (regexCity.test(temp)) {
+      locationForApi = `name=${temp}`;
+    }
+  }
+
+  return locationForApi;
+};
+
+export default ResolveSearchInput;
