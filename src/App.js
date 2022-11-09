fd317886105b0a8269a6bcac865280d4b1b5d349
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-11-09 07:06:11 -0500

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Form, Button } from 'react-bootstrap';
+
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column app-container">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container fluid>
+              <LinkContainer to="/">
+                <Navbar.Brand>Weather App</Navbar.Brand>
+              </LinkContainer>
+              <Navbar.Toggle aria-controls="navbarSearch" />
+              <Navbar.Collapse id="navbarSearch" className="my-2">
+                <Form className="d-flex ms-auto">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container></Container>
+        </main>
+        <footer className="text-center">
+          <a href="https://github.com/KLyamzin/weather-app-challenge-react">
+            GitHub Repo.
+          </a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
