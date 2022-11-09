b3650e224cd6225452e681684dd191282a5fc42a
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-11-09 13:44:03 -0500

    Added code description resolve module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
-import { Navbar, Container, Form, Button } from 'react-bootstrap';
+import { Navbar, Container, Form, Button, Table } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { BrowserRouter, Router, Routes } from 'react-router-dom';
 import LocationApiCall from './utils/LocationApiCall';
 import ResolveSearchInput from './utils/ResolveSearchInput';
 import WeatherApiCall from './utils/WeatherApiCall';
+import WeatherCodes from './utils/WeatherCodes';
 
 function App() {
+  // current input data
   const [location, setLocation] = useState('');
+  const [weather, setWeather] = useState({});
   const getWeather = (e) => {
     e.preventDefault();
     // get the location parameter for initial API call to get lat. & lon.
     const searchParameter = ResolveSearchInput(location); // returns something like: name=11234
 
-    //get the lat & long parameters
+    //get the lat & long parameters and get weather data object
     const getLatAndLonParameters = async () => {
       try {
         let data = await LocationApiCall(searchParameter);
-        let weatherData = await WeatherApiCall(data);
-        console.log(weatherData);
+        await WeatherApiCall(data).then((object) => setWeather(object));
+        // console.log(weather);
       } catch (error) {
         console.log(error);
       }
@@ -56,7 +59,41 @@ function App() {
           </Navbar>
         </header>
         <main>
-          <Container></Container>
+          <Container>
+            <Table striped bordered hover>
+              {/* <thead>
+                <tr>
+                  <th>#</th>
+                  <th>latitude</th>
+                  <th>longitude</th>
+                  <th>timezone</th>
+                </tr>
+              </thead> */}
+              <tbody>
+                <tr>
+                  <td>Coordinates</td>
+                  <td>latitude: {weather.latitude}</td>
+                  <td>longitude: {weather.longitude}</td>
+                  <td>
+                    timezone: {weather.timezone},{' '}
+                    {weather.timezone_abbreviation}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Temperature</td>
+                  {/* <td>{weather.current_weather.temperature}</td> */}
+                  {/* <td>Weathercode: {weather.current_weather.weathercode}</td> */}
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Wind</td>
+                  {/* <td>Direction: {weather.current_weather.winddirection}</td> */}
+                  {/* <td>Speed: {weather.current_weather.windspeed}</td> */}
+                  <td>Code: {WeatherCodes(77)}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Container>
         </main>
         <footer className="text-center">
           <a href="https://github.com/KLyamzin/weather-app-challenge-react">
--- src/utils/WeatherApiCall.js
+++ src/utils/WeatherApiCall.js
@@ -1,11 +1,13 @@
 const WeatherApiCall = async ({ lat, lon, name }) => {
-  const URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,weathercode&daily=weathercode&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`;
+  const URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`;
+  console.log(URL);
   const res = await fetch(URL);
   const data = await res.json();
+  console.log(data);
   const {
     timezone,
     timezone_abbreviation,
-    current_weather,
+    // current_weather,
     latitude,
     longitude,
     hourly_units,
@@ -13,7 +15,7 @@ const WeatherApiCall = async ({ lat, lon, name }) => {
   return new Object({
     timezone,
     timezone_abbreviation,
-    current_weather,
+    // current_weather,
     latitude,
     longitude,
     hourly_units,
